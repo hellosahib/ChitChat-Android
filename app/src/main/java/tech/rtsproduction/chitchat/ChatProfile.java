8cf67c3397ef53fdfc549721627f1b4cb231facc
Sahib <sahib1746.cse16@chitkara.edu.in>
2018-09-16 19:45:48 +0530

    Commit 4.0

---
+++ app/src/main/java/tech/rtsproduction/chitchat/ChatProfile.java
@@ -0,0 +1,32 @@
+package tech.rtsproduction.chitchat;
+
+public class ChatProfile {
+
+    private String profileName;
+    private String profileLastSeen;
+    private String profileDisplayImage;
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getProfileLastSeen() {
+        return profileLastSeen;
+    }
+
+    public void setProfileLastSeen(String profileLastSeen) {
+        this.profileLastSeen = profileLastSeen;
+    }
+
+    public String getProfileDisplayImage() {
+        return profileDisplayImage;
+    }
+
+    public void setProfileDisplayImage(String profileDisplayImage) {
+        this.profileDisplayImage = profileDisplayImage;
+    }
+}
